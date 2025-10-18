da8aefc02b2a5a7161e434c726d59ad007ba24aa
Sherenkov Ivan <sherenkov12@gmail.com>
2025-10-18 14:47:37 +0500

    Task1

--- Tyuiu.SherenkovIR.Sprint3.Task1.V24.Lib/DataService.cs
+++ Tyuiu.SherenkovIR.Sprint3.Task1.V24.Lib/DataService.cs
@@ -9,8 +9,10 @@ namespace Tyuiu.SherenkovIR.Sprint3.Task1.V24.Lib
 
             for (int i = startValue; i <= stopValue; i++)
             {
+              
                 double denominator = 6 + Math.Pow(value, i);
-                double term = 2.0 / denominator;  
+                double fraction = 2.0 / denominator;
+                double term = Math.Pow(fraction, i); 
                 sumSeries += term;
             }
             return Math.Round(sumSeries, 3);
