cc941e08b32670f2c709cb395c0bf6093300bd21
Sherenkov Ivan <sherenkov12@gmail.com>
2025-10-20 12:15:11 +0500

    Task4

---
+++ Tyuiu.SherenkovIR.Sprint3.Task4.V10.Lib/DataService.cs
@@ -0,0 +1,25 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+namespace Tyuiu.SherenkovIR.Sprint3.Task4.V10.Lib
+{
+    public class DataService : ISprint3Task4V10
+    {
+        public double Calculate(int startValue, int stopValue)
+        {
+            double res = 0;
+            for (int x = startValue; x <= stopValue; x++)
+            {
+                if (x == 0)
+                {
+                    break;
+                }
+                else
+                {
+                    double y = (Math.Sin(x) - x) / x;
+                }
+
+
+            }
+            return Math.Round(res, 3);
+        }
+    }
+}
---
+++ Tyuiu.SherenkovIR.Sprint3.Task4.V10.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.SherenkovIR.Sprint3.Task4.V10.Lib;
+namespace Tyuiu.SherenkovIR.Sprint3.Task4.V10.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+            int startValue = -5;
+            int stopValue = 5;
+
+            double res = ds.Calculate(startValue, stopValue);
+
+            double wait = 0;
+            Assert.AreEqual(res, wait);
+
+        }
+    }
+}
